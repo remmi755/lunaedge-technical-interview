1d78cc05ba6247db8a2d6006c97055ef6ae69234
remmi755 <90457721+remmi755@users.noreply.github.com>
2024-01-25 17:52:21 +0200

    App.tsx/add: #23
    add ThemeSwitcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./index.css";
 import Header from "./components/Header";
 import SelectPokemons from "./components/SelectPokemons";
+import ThemeSwitcher from "./components/ThemeSwitcher";
 function App() {
   return (
-    <div>
+    <div className="relative">
+      <div className="flex absolute top-6 right-2 justify-end items-center mr-8 mb-2">
+        <ThemeSwitcher />
+      </div>
       <Header />
       <SelectPokemons />
     </div>
