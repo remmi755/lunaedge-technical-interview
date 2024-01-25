964bf4865f4d0fa4113649013978588fb492bd86
remmi755 <90457721+remmi755@users.noreply.github.com>
2024-01-25 16:22:17 +0200

    Button/add:#6

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,115 @@
+import { Meta, StoryFn } from "@storybook/react";
+import { ButtonProps } from "./Button";
+import { action } from "@storybook/addon-actions";
+import Button from "./Button";
+import "../../App.css";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      type: "string",
+      defaultValue: "base",
+      options: ["xs", "sm", "base", "lg", "xl"],
+      control: { type: "radio" },
+    },
+    variant: {
+      type: "string",
+      defaultValue: "primary",
+      options: ["primary", "secondary", "outline", "text", "light", "dark"],
+      control: { type: "radio" },
+    },
+    onClick: action("clicked"),
+  },
+  parameters: {
+    layout: "centered",
+  },
+} as Meta;
+
+const Template: StoryFn<ButtonProps> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  type: "button",
+  text: "Button",
+  variant: "primary",
+  size: "lg",
+};
+
+export const Size = {
+  render: (args: ButtonProps) => (
+    <div className="flex justify-between items-start gap-8 p-2 m-auto">
+      <div className="flex flex-col">
+        <label className="text-center text-black mb-2">XS</label>
+        <Primary variant="primary" size="xs" text="Button" />
+      </div>
+      <div className="flex flex-col">
+        <label className="text-center text-black mb-2">SM</label>
+        <Primary variant="primary" size="sm" text="Button" />
+      </div>
+      <div className="flex flex-col">
+        <label className="text-center text-black mb-2">BASE</label>
+        <Primary variant="primary" size="base" text="Button" />
+      </div>
+      <div className="flex flex-col">
+        <label className="text-center text-black mb-2">LG</label>
+        <Primary variant="primary" size="lg" text="Button" />
+      </div>
+      <div className="flex flex-col">
+        <label className="text-center text-black mb-2">XL</label>
+        <Primary variant="primary" size="xl" text="Button" />
+      </div>
+    </div>
+  ),
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  type: "button",
+  text: "Button",
+  variant: "primary",
+  size: "lg",
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  disabled: true,
+  type: "button",
+  text: "Secondary Button",
+  variant: "secondary",
+  size: "lg",
+};
+
+export const Light = Template.bind({});
+Light.args = {
+  type: "button",
+  text: "Light",
+  variant: "light",
+  size: "lg",
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+  type: "button",
+  text: "Dark",
+  variant: "dark",
+  size: "lg",
+};
+
+export const Text = Template.bind({});
+Text.args = {
+  type: "button",
+  text: "Text Button",
+  variant: "text",
+  size: "lg",
+};
+
+export const Outline = Template.bind({});
+Outline.args = {
+  type: "button",
+  text: "Outline Button",
+  variant: "outline",
+  size: "lg",
+};
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import "../../App.css";
+import cn from "classnames";
+
+import IconStar from "../Icons/IconStar";
+import ChevronDown from "../Icons/ChevronDown";
+import { ReactNode } from "react";
+
+export interface ButtonProps {
+  children?: ReactNode;
+  type?: "button" | "reset" | "submit";
+  text: ReactNode;
+  variant: string;
+  size: string;
+  disabled?: boolean;
+  onClick?: (() => void) | any;
+}
+
+const children = (text: ReactNode): ReactNode => {
+  return (
+    <div className={`flex justify-between items-center `}>
+      <IconStar />
+      <p className="px-1">{text}</p>
+      <ChevronDown />
+    </div>
+  );
+};
+
+const Button: React.FC<ButtonProps> = ({
+  type,
+  disabled,
+  text,
+  size,
+  variant,
+  onClick,
+}) => {
+  const btnClass = cn(`button px-2`, `button-${size} button-${variant}`);
+
+  return (
+    <button
+      disabled={disabled}
+      type={type}
+      className={btnClass}
+      onClick={onClick}
+    >
+      {children(text)}
+    </button>
+  );
+};
+
+export default Button;
